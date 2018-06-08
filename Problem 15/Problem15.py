166c03d7fa3cb3f90c31252e5f0e8c187c5d2fe4
KelseyMcHenry <kelseymchenry@gmail.com>
2018-06-08 12:57:07 -0400

    15

---
+++ Problem 15/Problem15.py
@@ -0,0 +1,16 @@
+"""
+Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+
+How many such routes are there through a 20×20 grid?
+"""
+
+from itertools import permutations
+from math import factorial
+"""
+formula for all possible permutations
+P(n,r)=n!/(n−r)!
+(n-r)! = 1 when n = r, simplified here
+divided by all possible permutations of the set of 20 rights and 20 downs to account for repetitions
+"""
+print(int(factorial(40) / (factorial(20) * factorial(20))))
+
